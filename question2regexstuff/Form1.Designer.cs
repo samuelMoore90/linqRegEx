574b9723dd30058aa3d9117bd5f921f3e36b0862
samuelmoore32 <samuelmoore32@gmail.com>
2016-10-12 19:53:22 +0100

    first commit

---
+++ question2regexstuff/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace question2regexstuff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnFirstName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnConferenceId = new System.Windows.Forms.TextBox();
+            this.btnOrganistation = new System.Windows.Forms.TextBox();
+            this.btnSurname = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Confrerence Registration";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ConferenceId";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Organisation";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(102, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Surname";
+            // 
+            // btnFirstName
+            // 
+            this.btnFirstName.Location = new System.Drawing.Point(208, 56);
+            this.btnFirstName.Name = "btnFirstName";
+            this.btnFirstName.Size = new System.Drawing.Size(100, 20);
+            this.btnFirstName.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(102, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "First Name";
+            // 
+            // btnConferenceId
+            // 
+            this.btnConferenceId.Location = new System.Drawing.Point(208, 146);
+            this.btnConferenceId.Name = "btnConferenceId";
+            this.btnConferenceId.Size = new System.Drawing.Size(100, 20);
+            this.btnConferenceId.TabIndex = 7;
+            // 
+            // btnOrganistation
+            // 
+            this.btnOrganistation.Location = new System.Drawing.Point(208, 120);
+            this.btnOrganistation.Name = "btnOrganistation";
+            this.btnOrganistation.Size = new System.Drawing.Size(100, 20);
+            this.btnOrganistation.TabIndex = 8;
+            // 
+            // btnSurname
+            // 
+            this.btnSurname.Location = new System.Drawing.Point(208, 94);
+            this.btnSurname.Name = "btnSurname";
+            this.btnSurname.Size = new System.Drawing.Size(100, 20);
+            this.btnSurname.TabIndex = 9;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(208, 175);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 10;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(327, 175);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 11;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(30, 208);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(422, 198);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 430);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnSurname);
+            this.Controls.Add(this.btnOrganistation);
+            this.Controls.Add(this.btnConferenceId);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnFirstName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Conference Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox btnFirstName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox btnConferenceId;
+        private System.Windows.Forms.TextBox btnOrganistation;
+        private System.Windows.Forms.TextBox btnSurname;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ question2regexstuff/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Configuration;
+using System.IO;
+
+
+
+namespace question2regexstuff
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string confernceIdregEx =
+               ConfigurationManager.AppSettings["conferenceId"];
+            string firstnamereg = ConfigurationManager.AppSettings["names"];
+            string lastnameregex = ConfigurationManager.AppSettings["names"];
+            string conferenceId = btnConferenceId.Text;
+            string fn = btnFirstName.Text;
+            string ln = btnSurname.Text;
+
+            if(Regex.IsMatch(conferenceId, confernceIdregEx))
+            {
+               // MessageBox.Show("Valid Entry");
+               if(Regex.IsMatch(ln, lastnameregex) && Regex.IsMatch(fn, firstnamereg))
+                {
+                    // write to file
+                    StreamWriter sw = new StreamWriter(@"C:\Users\samue\Desktop\advprogramming\question2regexstuff\question2regexstuff\people.txt");
+                    
+                    sw.WriteLine("first Name:" + fn);
+                    sw.WriteLine("Lastname: " + ln);
+                    sw.WriteLine("ConferenceID: " + conferenceId);
+                    sw.WriteLine("Organisation: " + btnOrganistation.Text);
+                    sw.Close();
+
+                    using(Stream f = File.Open(@"C:\Users\samue\Desktop\advprogramming\question2regexstuff\conference.bin", FileMode.OpenOrCreate))
+                    {
+                        
+                        using(BinaryWriter bw = new BinaryWriter(f))
+                        {
+                            try
+                            {
+                                bw.Write(fn);
+                                bw.Write(ln);
+                                bw.Write(conferenceId);
+                                bw.Write(btnOrganistation.Text);
+                                MessageBox.Show("Valid Entry, Contact written to binary file");
+                            }
+                            catch(Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                           
+                        }
+                    }
+
+                }
+               else if(Regex.IsMatch(fn, firstnamereg))
+                {
+                    MessageBox.Show("lastname wrong");
+                    btnSurname.Focus();
+
+                }
+                else if(Regex.IsMatch(ln, lastnameregex))
+                {
+                    MessageBox.Show("firstname wrong");
+                    btnFirstName.Focus();
+
+                }
+               else
+                {
+                    MessageBox.Show("Both Names are wrong");
+                    btnSurname.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Conference Id");
+                btnConferenceId.Focus(); ;
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            using (Stream f = File.Open(@"C:\Users\samue\Desktop\advprogramming\question2regexstuff\conference.bin", FileMode.Open))
+            {
+                using(BinaryReader br = new BinaryReader(f))
+                {
+                   string fn =  br.ReadString();
+                   string ln = br.ReadString();
+                    string o = br.ReadString();
+                    var names = fn + " " + ln + " " + o + Environment.NewLine;
+                    
+                    richTextBox1.AppendText(names);
+                }
+            }
+        }
+    }
+}
